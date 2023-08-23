d598ae5a130146fe023b78c818b533eb8856566e
Derek Belloni <dbelloni@compunet.biz>
2023-08-23 07:58:48 -0600

    basic chat functionality between two different browsers is operational

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,41 @@ import ChatBox from './components/ChatBox.vue'
 import Navbar from './components/Navbar.vue'
 import TextArea from './components/TextArea.vue'
 import SocketService from './services/socketio.service.js'
-import { onMounted, onBeforeUnmount, reactive } from 'vue'
+import { socket, state } from "./services/socket"
+import { onMounted, watch, reactive, ref } from 'vue'
 
 let msgEvent =  reactive([]);
+let connected = ref(false)
+let message = ref("")
+
 
 onMounted(() => {
-  SocketService.setupSocketConnection();
+  msgEvent = state.receivedMessages
+  connected = state.connected
+  console.log('message event: ', msgEvent)
 })
 
-onBeforeUnmount(() => {
-  SocketService.disconnect();
+watch(state.receivedMessages, (newValue, oldValue) => {
+  console.log("New message", newValue);
 })
+
+function connect() {
+   socket.connect()
+   connected = true;
+   console.log('connected: ', connected)
+}
+
+function disconnect() {
+  socket.disconnect()
+  connected = false;
+  console.log('connected: ', connected)
+}
 
 function sendMessage(message) {
-  SocketService.emit(message)
+  socket.emit('message', message)
 }
+
+
 
 </script>
 
@@ -27,6 +47,22 @@ function sendMessage(message) {
     <Navbar/>
   </header>
   <main>
+    <div class="mx-4 my-4">
+      <div class="flex flex-row items-center">
+        <div class="space-x-2">
+          <button @click="connect()" class="rounded shadow-sm border border-green-600 p-2 text-green-400">Connect</button>
+          <button @click="disconnect()" class="rounded shadow-sm border border-red-600 p-2 text-red-400">Disconnect</button>
+        </div>
+        <div class="ml-4 mr-8">
+          <span :class="connected ? 'text-green-500 font-medium' : 'text-red-500 font-medium' ">Connected: {{connected}}</span>
+        </div>
+      </div>
+      <div class="flex flex-col">
+        <div v-for="message in state.receivedMessages">
+          {{ message }}
+        </div>
+      </div>
+    </div>
     <ChatBox/>
   </main>
   <footer>
